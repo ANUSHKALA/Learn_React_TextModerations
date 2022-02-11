c084fba989b777d81e409cb33e6703198a75529b
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-02-11 15:55:42 +0530

    diff modes

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -5,12 +5,37 @@ import React,{useState} from 'react';
 function App() {
   const[mode,setMode] = useState('light');
 
-const toggleMode = () => {
+const toggleDarkMode = () => {
   if(mode == 'light'){
     setMode('dark');
     document.body.style.backgroundColor = '#383f45';
   }
-  else if(mode == 'dark'){
+  else if(mode == 'dark' || 'peach' || 'rainbow'){
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+  }
+  
+}
+
+const toggleRainbowMode = () => {
+  if(mode === 'light' ){
+    setMode('rainbow');
+    document.body.style.backgroundColor = 'orange';
+  }
+  else if(mode == 'rainbow' || 'dark' || 'peach' ||'light'){
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+  }
+  
+}
+
+
+const togglePeachMode = () => {
+  if( mode == 'light'){
+    setMode('peach');
+    document.body.style.backgroundColor = 'pink';
+  }
+  else if(mode == 'peach' || 'light' || 'dark' || 'rainbow'){
     setMode('light');
     document.body.style.backgroundColor = 'white';
   }
@@ -19,9 +44,10 @@ const toggleMode = () => {
 
   return (
     <>
-      <Navbar  mode = {mode} toggleMode={toggleMode} /> 
+      <Navbar  mode = {mode} toggleDarkMode={toggleDarkMode} togglePeachMode={togglePeachMode} toggleRainbowMode={toggleRainbowMode}/> 
       <div className = 'container'>
         <TextForm heading = "Enter the text here" mode = {mode}/>
+        
       </div>
   </>
   );
--- my-app-2/src/Components/Navbar.js
+++ my-app-2/src/Components/Navbar.js
@@ -25,9 +25,17 @@ export default function Navbar(props) {
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form> */}
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <input className="form-check-input" onClick={props.toggleDarkMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Change Mode</label>
-      </div>      
+      </div>
+      <div className={`form-check form-switch text-${props.mode==='dark'||'light'||'rainbow'?'#d63384;':'light'}`}>
+        <input className="form-check-input" onClick={props.togglePeachMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Peach Mode</label>
+      </div>
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+        <input className="form-check-input" onClick={props.toggleRainbowMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Rainbow Mode</label>
+      </div>       
     </div>
   </div>
 </nav>
